2fd758b7b261afbf4e6521549833d5ce89d68a03
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2021-01-02 21:59:33 +0100

    Add config

--- config/hasher.php
+++ config/hasher.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    //
+    'salt' => [
+        'sha512' => env('SHA512_SALT'),
+        'sha256' => env('SHA256_SALT'),
+        'md5' => env('MD5_SALT'),
+        'sha1' => env('SHA1_SALT'),
+    ],
 ];
